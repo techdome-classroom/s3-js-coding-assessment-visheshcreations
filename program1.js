8f5010a9f6bfe4208fb98f071374ef9363af29ea
vishesh vishwakarma <90443558+visheshcreations@users.noreply.github.com>
2024-09-26 00:28:42 +0530

    question1 sloved

--- program1.js
+++ program1.js
@@ -2,8 +2,30 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    const stack = [];
+
+    for (const char of s) {
+        if (bracketMap[char]) {
+            const topElement = stack.length ? stack.pop() : '#';
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+
+
+
 };
 
 module.exports = { isValid };
